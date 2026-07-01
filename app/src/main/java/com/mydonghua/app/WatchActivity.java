7ffbb41bd554e5c4b33c8757f9853699b4048f96
NSR-XINE <nasironfire07@gmail.com>
2026-07-01 13:48:37 +0400

    show status/navigation bars in WatchActivity (streaming player)

--- app/src/main/java/com/mydonghua/app/WatchActivity.java
+++ app/src/main/java/com/mydonghua/app/WatchActivity.java
@@ -174,21 +174,22 @@ public class WatchActivity extends AppCompatActivity {
             android.view.Window window = getWindow();
             if (window == null) return;
 
+            window.setStatusBarColor(android.graphics.Color.BLACK);
+            window.setNavigationBarColor(android.graphics.Color.BLACK);
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 window.setDecorFitsSystemWindows(false);
                 WindowInsetsController ctrl = window.getInsetsController();
                 if (ctrl != null) {
-                    ctrl.hide(WindowInsets.Type.systemBars());
-                    ctrl.setSystemBarsBehavior(
-                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                    ctrl.show(WindowInsets.Type.systemBars());
                 }
             } else {
                 View decorView = window.getDecorView();
                 if (decorView != null) {
                     decorView.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     );
                 }
             }
